8ae169b181408ab7f0e1d3b9c9f3b46f84bbda51
Pasumarthi Rutwik <62496029+rutwik2001@users.noreply.github.com>
2022-03-24 13:30:28 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+import "regenerator-runtime/runtime";
+
+function MyApp({ Component, pageProps }) {
+  const supportedChainIds = [80001, 4];
+
+  const connectors = {
+    injected: {},
+  };
+
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
+}
+
+export default MyApp;
